f970b17b104ee3a9e9040b43639c29172730a5f3
Philip Frerk <philip.frerk@icloud.com>
2024-11-24 19:24:53 +0100

    fix if

--- tools/check-unlabeled-issues.sh
+++ tools/check-unlabeled-issues.sh
@@ -38,7 +38,7 @@ for FILTER in "$TYPE_LABELS_FILTER" "$PRIORITY_LABELS_FILTER"; do
   fi
 done
 
-if [ "$HAS_ISSUES_MISSING_LABELS == true" ]; then
+if [ "$HAS_ISSUES_MISSING_LABELS = true" ]; then
   LABEL_CHECK_ACTION="Label check action"
   LABEL_CHECK_ISSUE_EXISTS=$(gh search issues --label "$LABEL_CHECK_ACTION" --repo $REPO --json number) || { echo "Failed to fetch existing label check issue"; exit 1; }
   ISSUE_NUMBER=$(echo "$LABEL_CHECK_ISSUE_EXISTS" | jq -r '.[].number')
